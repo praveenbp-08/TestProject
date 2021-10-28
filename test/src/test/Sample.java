b68fe2f9d86c167e4827a2ac6a396cff2e0a3ea6
praveenbp-08 <93185273+praveenbp-08@users.noreply.github.com>
2021-10-28 12:43:44 +0530

    full stop

--- test/src/test/Sample.java
+++ test/src/test/Sample.java
@@ -3,7 +3,7 @@ package test;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("hai capgemini");
+		System.out.println("hai capgemini.");
 		// TODO Auto-generated method stub
 
 	}
